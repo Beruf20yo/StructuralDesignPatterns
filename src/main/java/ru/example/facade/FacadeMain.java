6159f40a7077085a05dd06e93776a8e56df60a40
RinatG <rinat_galimov_1992@mail.ru>
2023-10-30 18:52:29 +0300

    added Facade pattern

--- src/main/java/ru/example/facade/FacadeMain.java
+++ src/main/java/ru/example/facade/FacadeMain.java
@@ -2,6 +2,8 @@ package ru.example.facade;
 
 public class FacadeMain {
     public static void main(String[] args) {
-
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1100100","11001000"));// 100 + 200 = 100101100
+        System.out.println(bins.mult("1100100","11001000"));// 100 * 200 = 100111000100000
     }
 }
